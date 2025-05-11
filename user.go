73b9238b03d9fb9d3c3323080aad0c096e8392c5
曹阳 <1791200683@qq.com>
2025-05-11 11:05:45 +0800

    feat:用户修改用户名

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 // 客户端用户结构体
 type User struct {
@@ -40,15 +43,33 @@ func (this *User) SendMsg(msg string) {
 
 // 用户消息处理
 func (this *User) DoMessage(msg string) {
+	stringArr := strings.Split(msg, " ")
 	if msg == "im online\n" {
 		// 查询当前所有在线用户
 		this.server.mapLock.Lock()
 		for _, user := range this.server.OnlineMap {
-			onlineMsg := "[" + user.Addr + "]" + ":" + "在线...\n"
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
 			// 写到当前用户的channel
 			this.SendMsg(onlineMsg)
 		}
 		this.server.mapLock.Unlock()
+	} else if stringArr[1] == "rename" {
+		// 去掉新用户名后的空格
+		newName := stringArr[2][:len(stringArr[2])-1]
+		// 判断用户是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("该用户名已存在！\n")
+		} else {
+			this.server.mapLock.Lock()
+			// 删除map中的原用户，新增新名称的用户
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("更新用户名成功：" + this.Name + "\n")
+		}
 	} else {
 		this.server.Broadcast(this, msg)
 	}
